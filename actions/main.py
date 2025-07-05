ce50a7e579996a52496201afaea101572797a67b
mithrajith <ksmithun94@gmail.com>
2025-07-05 18:24:51 +0530

    new_with_code

--- actions/main.py
+++ actions/main.py
@@ -6,4 +6,4 @@ def echo(text):
 iface = gr.Interface(fn=echo, inputs="text", outputs="text", title="Echo App")
 
 if __name__ == "__main__":
-    iface.launch()
+    iface.launch(server_name="0.0.0.0", server_port=7860, share=True)
