1843bb4eb25e393a2223d1c16038ca2b12fef84f
mithrajith <ksmithun94@gmail.com>
2025-07-05 18:32:05 +0530

    k

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import gradio as gr
-
+# main.py (modified)
 def echo(text):
     return f"You entered: {text}"
 
-iface = gr.Interface(fn=echo, inputs="text", outputs="text", title="Echo App")
-
 if __name__ == "__main__":
-    iface.launch(server_name="0.0.0.0", server_port=7860, share=True)
+    print(echo("Mithun"))
